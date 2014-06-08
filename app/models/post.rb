98f782a1d56da2466379f9334912b31827bbca8f
bschmaltz <benschmaltz@gmail.com>
2014-06-08 00:18:15 -0400

    get rid of defalut reveal for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,3 @@
 class Post < ActiveRecord::Base
-  after_initialize :default_values
-
   validates_length_of :content, :minimum => 1, :maximum => 200, :allow_blank => false
-
-
-  private
-  def default_values
-    self.revealed ||= false
-  end
 end
