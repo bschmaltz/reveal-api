5ccb1fd7fc14b7c694588cc61443e7df682d4f09
bschmaltz <benschmaltz@gmail.com>
2014-06-16 16:44:22 -0400

    heroku setup stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140613043810) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "followers", force: true do |t|
     t.integer  "user_id"
     t.integer  "followed_user_id"
