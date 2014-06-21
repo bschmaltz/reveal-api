b6e779672c35accad1f204ffdaeadf97b6e0d45c
bschmaltz <benschmaltz@gmail.com>
2014-06-21 02:41:55 -0400

    post vote front end fix

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -26,7 +26,7 @@ class VotesController < ApplicationController
     @user = authenticate_token
     @vote = Vote.find_by_user_id_and_post_id(vote_params[:user_id], vote_params[:post_id])
     @post = Post.find(vote_params[:post_id])
-    if !@user.nil? and !@vote.nil? and @user.id==vote_params[:user_id] and @vote.user_id==@user.id and @post.user_id!=@user.id and @vote.update_attributes(vote_params)
+    if !@user.nil? and !@vote.nil? and !@post.nil? and @user.id==vote_params[:user_id] and @vote.user_id==@user.id and @post.user_id!=@user.id and @vote.update_attributes(vote_params)
       @result = true
     else
       @result = false
